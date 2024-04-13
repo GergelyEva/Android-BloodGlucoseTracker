d1beab45a32d4e651a538e21b07ed8c4bb285c56
GergelyEva <141821679+GergelyEva@users.noreply.github.com>
2024-04-13 17:51:12 +0300

    Adding server, readme.

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-"# Android-BloodGlucoseTracke" 
+<h3 align="center">Blood Glucose Tracker app</h3>
+<p align="center">This app was a project for the Android course. The app is meant to help diabetics to keep track of their glucose values.
+The application is developed in Java, the server is implemented in C#, and phpMyAdmin is used for administrating the MySQL database.</p>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Contents</summary>
+  <ol>
+    <li>Blood Glucose App
+      <ol>
+        <li><a href="#Description">Description</a></li>
+        <li><a href="#MainActivity">MainActivity</a></li>
+        <li><a href="#LoginActivity">LoginActivity</a></li>
+        <li><a href="#HomeFragment">HomeFragment</a></li>
+        <li><a href="#BloodSugarToday">BloodSugarToday</a></li>
+        <li><a href="#NotificationForegroundService">NotificationForegroundService</a></li>
+        <li><a href="#GraphFragment">GraphFragment</a></li>
+        <li><a href="#Constants">Constants</a></li>
+      </ol>
+    </li>
+    <li>Server and Database 
+      <ol>
+        <li><a href="#Server">Server</a></li>
+        <li><a href="#Database">Database</a></li>
+      </ol>
+    </li>
+  </ol>
+</details>
+
+
+### Description
+
+The Blood Glucose Tracker is an app, where the user can enter its daily blood glucose values, see the values in a graph, send the values to a doctor. 
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### MainActivity
+
+The MainActivity is responsible for managing the app's navigation, handling the display of the fragments.
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### LoginActivity
+
+This class is responsible for the login functionality of the app. The user needs to input a username, password and doctors email. After the login, the notification foreground service starts.
+
+![My Image](img/login.png)
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### HomeFragment
+This class is responsible for displaying a short welcome message to the home screen.
+
+![My Image](img/home.png)
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### BloodSugarToday
+
+This class is a fragment, where the user can write their blood sugar values ar different times in the day. At the end of the day, the information gets sent to the server, which sends it to the database. 
+
+![My Image](img/BloodSugarValueAdd.png)
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### NotificationForegroundService
+
+This class manages the foreground service for displaying notifications at specific times in the app. The notification times are set in the BloodSugarToday. 
+
+![My Image](img/notification.png)
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### GraphFragment
+
+This class is responsible for displaying a graph of the users blood glucose values over time. It also contains a button to send this values to a doctor, whose email was noted in login info.
+
+![My Image](img/graph.png)  ![My Image](img/mail.jpg) 
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### Constants
+This class contains the static variables used in the app.
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### Server
+
+Server application written in C#. It listens for incoming TCP connections, processes client requests, and interacts with a MySQL database. The Blood Glucose Tracker app sends the data with the first word as its action: retrieve or insert, then based on the action word the specific method is called.
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
+
+### Database
+
+The Database is managed through phpMyAdmin. It contains the following columns: Username, Password, Email, Date, b7am (blood glucose levels measured at 7 am),b1pm (blood glucose levels measured at 1 pm), b7pm (blood glucose levels measured at 7 pm), bs (blood glucose levels measured before sleep.
+
+<p align="right">(<a href="#readme-top">Back to Top</a>)</p>
---
+++ Serverbloodg/Serverbloodg/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+class Serverbloodg
+{
+    private static readonly string connectionString = "datasource=IP;port=PORT;username=root;password=;database=DATABASENAME";
+
+    static void Main()
+    {
+        TcpListener server = null;
+
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("IP"); 
+            int port = 8000;
+
+            server = new TcpListener(ipAddress, port);
+            server.Start();
+
+            Console.WriteLine("Server started on " + ipAddress + ":" + port);
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+
+                Thread clientThread = new Thread(() => HandleClientCommunication(client));
+                clientThread.Start();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+
+    static void HandleClientCommunication(TcpClient client)
+    {
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            string message = ReadMessage(stream);
+            Console.WriteLine($"Received: " + message);
+            string responseMessage = ProcessParameters(message);
+            WriteMessage(stream, responseMessage);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error handling client: " + e.Message);
+        }
+        finally
+        {
+            client.Close();
+            Console.WriteLine("Server closed");
+        }
+    }
+
+    static string ReadMessage(NetworkStream stream)
+    {
+        byte[] data = new byte[256];
+        int bytesRead = stream.Read(data, 0, data.Length);
+        return Encoding.ASCII.GetString(data, 0, bytesRead);
+    }
+
+    static void WriteMessage(NetworkStream stream, string message)
+    {
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+    }
+
+    static string ProcessParameters(string message)
+    {
+        try
+        {
+            string[] words = message.Split('-');
+            string action = words[0];
+            string Username = words[1];
+            string Password = words[2];
+            string Email = words[3];
+            string date = words[4];
+            int b7am = int.Parse(words[5]);
+            int b1pm = int.Parse(words[6]);
+            int b7pm = int.Parse(words[7]);
+            int bs = int.Parse(words[8]);
+
+            if (action.Equals("INSERT"))
+            {
+                InsertUserData(Username, Password, Email, date, b7am, b1pm, b7pm, bs);
+                return "Data inserted successfully!";
+            }
+            else if (action.Equals("RETRIEVE"))
+            {
+                string userData = RetrieveUserData(Username, Password, Email);
+                return userData;
+            }
+            else
+            {
+                return "Invalid action specified!";
+            }
+        }
+        catch (Exception ex)
+        {
+            return "Error processing message: " + ex.Message;
+        }
+    }
+
+    static void InsertUserData(string Username, string Password, string Email, string date, int b7am, int b1pm, int b7pm, int bs)
+    {
+        try
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand("INSERT INTO `TABLENAME` (`Username`, `Password`, `Email`, `date`, `b7am`, `b1pm`, `b7pm`, `bs`) VALUES (@Username, @Password, @Email, @date, @b7am, @b1pm, @b7pm, @bs)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", Username);
+                    cmd.Parameters.AddWithValue("@Password", Password);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@date", date);
+                    cmd.Parameters.AddWithValue("@b7am", b7am);
+                    cmd.Parameters.AddWithValue("@b1pm", b1pm);
+                    cmd.Parameters.AddWithValue("@b7pm", b7pm);
+                    cmd.Parameters.AddWithValue("@bs", bs);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error inserting data: " + ex.Message);
+        }
+    }
+
+    static string RetrieveUserData(string Username, string Password, string Email)
+    {
+        try
+        {
+            StringBuilder data = new StringBuilder();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand("SELECT date, b7am, b1pm, b7pm, bs FROM TABLENAME WHERE Username = @Username AND Password = @Password", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", Username);
+                    cmd.Parameters.AddWithValue("@Password", Password);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string rowData = $"{reader.GetString("date")}-{reader.GetInt32("b7am")}-{reader.GetInt32("b1pm")}-{reader.GetInt32("b7pm")}-{reader.GetInt32("bs")}";
+                            data.AppendLine(rowData);
+                        }
+                    }
+                }
+                Console.WriteLine(data);
+
+            }
+
+            if (data.Length > 0)
+            {
+                return data.ToString();
+            }
+            else
+            {
+                return "No data found for the provided username and password";
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error retrieving user data: " + ex.Message);
+            return "Error retrieving user data: " + ex.Message;
+        }
+    }
+
+}
